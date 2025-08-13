fc8d76fea23df7eba723d7d5da5bf08e68679920
venkata-nimmagadda-maersk <venkata.nimmagadda@maersk@maersk.com>
2025-08-13 18:07:17 +0530

    random commit

--- src/main/java/com/example/ProductDemo/InventoryTransaction/InventoryTransactionRepo.java
+++ src/main/java/com/example/ProductDemo/InventoryTransaction/InventoryTransactionRepo.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InventoryTransactionRepo extends JpaRepository<InventoryTransaction, Integer> {
-
+//comment for branch
 }
